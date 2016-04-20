d50d16ab2b18116e8fd2d6553b2e1086b1e5d3a2
Sami Farhat <sami.kfarhat@gmail.com>
2016-04-20 22:23:17 +0100

    add include header that contains all constant strings

---
+++ AdvWallpapers/AdvWallpapers.h
@@ -0,0 +1,19 @@
+//
+//  AdvWallpapers.h
+//  AdvWallpapers
+//
+//  Created by Sami Farhat on 20/04/2016.
+//  Copyright © 2016 Sami Farhat. All rights reserved.
+//
+
+#ifndef AdvWallpapers_h
+#define AdvWallpapers_h
+
+#define STATUS_CONTROLLER                   @"StatusWindowController"
+#define MAIN_CONTROLLER                     @"MainWindowController"
+#define STORY_NAME                          @"Main"
+#define APPLESCRIPT_CURRENT_DESKTOP         @"CurrentDesktop"
+#define APPLESCRIPT_SET_DESKTOP_PICTURE     @"SetDesktopPicture"
+#define NOTIFICATION_SHOW_MAIN_CONTROLLER   @"showMainViewController"
+
+#endif /* AdvWallpapers_h */
